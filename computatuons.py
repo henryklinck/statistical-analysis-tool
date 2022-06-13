d5e94aa726834aa8ce9c90c57372c8c48336fc88
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-06-12 21:48:28 -0400

    demo commit 2

--- computatuons.py
+++ computatuons.py
@@ -1,1 +1,3 @@
 # ver.0 of computations - Determine line of best fit across all numerical variables & strength of correlation
+
+# The first step in this process is to determine a line of best fit between two variables and determine strength of correlation.
