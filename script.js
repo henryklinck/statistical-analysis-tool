8f64772e683ecb70352148bff5421cdeef3a3686
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-07-14 15:04:28 -0400

    re-started everything

---
+++ script.js
@@ -0,0 +1,48 @@
+let inputdata = document.getElementById("input-data");
+let csvfile = document.getElementById("csv-input");
+let numvar = document.getElementById("num-value-input");
+
+function csvToArray(str, delimiter = ",") {
+
+    // slice from start of text to the first \n index
+    // use split to create an array from string by delimiter
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+
+    // slice from \n index + 1 to the end of the text
+    // use split to create an array of each csv value row
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+    // Map the rows
+    // split values from each row into an array
+    // use headers.reduce to create an object
+    // object properties derived from headers:values
+    // the object passed as an element of the array
+    const arr = rows.map(function (row) {
+      const values = row.split(delimiter);
+      const el = headers.reduce(function (object, header, index) {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return el;
+    });
+
+    // return the array
+    return arr;
+  }
+
+
+inputdata.addEventListener('submit', function (e) {
+    e.preventDefault;
+    console.log("Form Submitted")
+    const input = csvfile.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const text = e.target.result;
+        const data = csvToArray(text);
+        document.getElementById("result").innerHTML = data;
+        document.write(JSON.stringify(data));
+      };
+      
+      reader.readAsText(input);
+    });
