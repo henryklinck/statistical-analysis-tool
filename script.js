cc41db71d47e11017bc39065120ef3defc87e9ad
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-07-15 12:04:15 -0400

    pearsons coeffiecient computations done and tested

--- script.js
+++ script.js
@@ -34,29 +34,30 @@ function csvToArray(str, numvar, delimiter = ",") {
 }
 
 function computePearsonsCoef(var1raw, var2raw) {
-     // assume numerical data
-     var var1type = "numerical";
-     var var2type = "numerical";
+    // assume numerical data and ordinal data (in order) or boolean catergorical data
+    var var1type = "numerical";
+    var var2type = "numerical";
 
-     var var1 = var1raw.slice(1);
-     var var2 = var2raw.slice(1);
+    var var1 = var1raw.slice(1);
+    var var2 = var2raw.slice(1);
 
-     return var1
-
-     if (isNaN(var1[0])) {
-        var1type = "categorical";
-        var numvar1 = [];
-        var varvalues1 = [];
-        for (let i=0; i < var1.length; i++) {
-            if (!(varvalues1.includes(var1[i]))) {
+    if (isNaN(var1[0])) {
+       var1type = "categorical";
+       var numvar1 = [];
+       var varvalues1 = [];
+       for (let i=0; i < var1.length; i++) {
+           if (!(varvalues1.includes(var1[i]))) {
                 varvalues1.push(var1[i]);
             }
             numvar1[i] = varvalues1.indexOf(var1[i]);
         }
+        if (varvalues1.length < 2) {
+            window.alert(var1raw[0] + "Has only 1 element")
+        }
         var1 = numvar1;
-     }
+    }
      
-     if (isNaN(var2[0])) {
+    if (isNaN(var2[0])) {
         var2type = "categorical";
         var numvar2 = [];
         var varvalues2 = [];
@@ -64,24 +65,42 @@ function computePearsonsCoef(var1raw, var2raw) {
             if (!(varvalues2.includes(var2[j]))) {
                 varvalues2.push(var2[j]);
             }
-            numvar2[j] = varvalues2.indexOf(var2[j] + 1);
+            numvar2[j] = varvalues2.indexOf(var2[j]);
+        }
+        if (varvalues2.length < 2) {
+            window.alert(var2raw[0] + "Has only 1 element")
         }
         var2 = numvar2;
-     }
+    }
 
-     var sumvar1 = 0;
-     var sumvar2 = 0;
+    var sumvar1 = 0;
+    var sumvar2 = 0;
 
-     for (let k=0; k < var1.length; k++) {
+    for (let k=0; k < var1.length; k++) {
         sumvar1 += parseInt(var1[k]);
         sumvar2 += parseInt(var2[k]);
-     }
+    }
 
-     var meanvar1 = (sumvar1 / var1.length);
-     var meanvar2 = (sumvar2 / var2.length);
+    var meanvar1 = (sumvar1 / var1.length);
+    var meanvar2 = (sumvar2 / var2.length);
 
-     return sumvar1
+    // Calculate sum[(x_i - x_hat)(y_i - y_hat)]
+    var numeratorsum = 0;
+    var denominatorsum1 = 0;
+    var denominatorsum2 = 0;
+    var var1_i = 0;
+    var var2_i = 0;
+    for (let p=0; p < var1.length; p++) {
+        var1_i = parseInt(var1[p]);
+        var2_i = parseInt(var2[p]);
+        numeratorsum += ((var1_i - meanvar1) * (var2_i - meanvar2));
+        denominatorsum1 += ((var1_i - meanvar1) ** 2);
+        denominatorsum2 += ((var2_i - meanvar2) ** 2);
+    }
+
+    var pcoefficient = (numeratorsum / ((denominatorsum1 ** (1/2)) * (denominatorsum2 ** (1/2))));
     
+    return pcoefficient.toFixed(4);
 }
 
 inputdata.addEventListener('submit', function (e) {
