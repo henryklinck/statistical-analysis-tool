42ad53f0a371d1f8fa56aacb2a46d4d9ace5ec81
HENRY KLINCK <henry.klinck@mail.utoronto.ca>
2022-07-15 00:16:10 -0400

    starting determine pearsons coefficient function, not done

--- script.js
+++ script.js
@@ -13,68 +13,74 @@ function csvToArray(str, numvar, delimiter = ",") {
     const rows = str.slice(str.indexOf("\n") + 1).split("\n");
     const lenrows = rows.length
 
-    // check if there are values in original data, not a perfect method
-    //if (((rows.length / numrows) / numvar) != 1) {
-    //    window.alert("ERROR: Wrong Num of Vars or Invalid Data (commas in data or improper format)")
-    //    return
-    //}
-
     var allvalues1 = [];
 
     for (let j=0; j < numvar; j++) {
-        allvalues1[j] = new Array
+        allvalues1[j] = new Array;
         allvalues1[j].push(headers[j]);
         for (let i=1; i<lenrows; i++) {
-            usablerow = rows[i].split(delimiter)
+            usablerow = rows[i].split(delimiter);
             allvalues1[j].push(usablerow[j]);
-        }
+        }        
     }
 
-    return allvalues1[1]
+    // check for proper input data 
+    if (allvalues1[0].length != allvalues1[(numvar - 1)].length) {
+        window.alert("Error")
+        return allvalues1[0];
+    }
 
-    // slice from start of text to the first \n index
-    // use split to create an array from string by delimiter
-    //const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-
-    // slice from \n index + 1 to the end of the text
-    // use split to create an array of each csv value row
-    //const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-
-    //var allvalues = [];
-
-    //for (let j = 0; j < headers.length; j++) {
-    //    allvalues.push(headers[j]);
-    //    for (let i = 1; i < rows.length; i++) {
-    //        allvalues[j].push(rows[i][j]);
-    //    }
-    //}
-
-    //return allvalues;
+    return allvalues1;
 }
 
+function computePearsonsCoef(var1raw, var2raw) {
+     // assume numerical data
+     var var1type = "numerical";
+     var var2type = "numerical";
 
-    // Map the rows
-    // split values from each row into an array
-    // use headers.reduce to create an object
-    // object properties derived from headers:values
-    // the object passed as an element of the array
-    /*const arr = rows.map(function (row) {
-      const values = row.split(delimiter);
-      const el = headers.reduce(function (object, header, index) {
-        object[header] = values[index];
-        return object;
-      }, {});
-      return el;
-    });
+     var var1 = var1raw.slice(1);
+     var var2 = var2raw.slice(1);
 
-    // return the array
-    return arr;
-  } */
+     if (isNaN(var1)) {
+        var1type = "categorical";
+        var numvar1 = [];
+        var varvalues1 = [];
+        for (let i=0; i < var1.length; i++) {
+            if (!(varvalues1.includes(var1[i]))) {
+                varvalues1.push(var1[i]);
+            }
+            numvar1[i] = varvalues1.indexOf(var1[i]);
+        }
+        var1 = numvar1;
+     }
+     
+     if (isNaN(var2)) {
+        var2type = "categorical";
+        var numvar2 = [];
+        var varvalues2 = [];
+        for (let j=0; j < var2.length; j++) {
+            if (!(varvalues2.includes(var2[j]))) {
+                varvalues2.push(var2[j]);
+            }
+            numvar2[j] = varvalues2.indexOf(var2[j]);
+        }
+        var2 = numvar2;
+     }
 
-/*function csvToArray1(str, num) {
-    // input csvfile, returns array of different *columns in csv file as arrays.
+     var sumvar1 = 0;
+     var sumvar2 = 0;
 
-}*/
+     for (let k=0; k < var1.length; k++) {
+        sumvar1 += var1[k];
+        sumvar2 += var2[k];
+     }
+
+     var meanvar1 = (sumvar1 / var1.length);
+     var meanvar2 = (sumvar2 / var2.length);
+
+     return meanvar1
+    
+}
 
 inputdata.addEventListener('submit', function (e) {
     e.preventDefault;
@@ -86,6 +92,7 @@ inputdata.addEventListener('submit', function (e) {
     reader.onload = function (e) {
         const text = e.target.result;
         const data = csvToArray(text, numvar.value);
+        var result = computePearsonsCoef(data[1], data[2]);
         
 
         // document.getElementById("result").innerHTML = data;
@@ -93,7 +100,7 @@ inputdata.addEventListener('submit', function (e) {
         // window.alert(JSON.stringify(data));
         // window.alert(data);
         // var basic_data = JSON.stringify(data[0]);
-        document.write(data);
+        document.write(result);
       };
       reader.readAsText(input);
       
